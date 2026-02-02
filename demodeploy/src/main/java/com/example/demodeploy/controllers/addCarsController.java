a9b3cb9ad9dbaa217d48da95ba2ed5995e073aa1
shubham182001 <shubham1822001@gmail.com>
2026-02-02 20:36:27 +0530

    cod repaired

--- demodeploy/src/main/java/com/example/demodeploy/controllers/addCarsController.java
+++ demodeploy/src/main/java/com/example/demodeploy/controllers/addCarsController.java
@@ -3,7 +3,6 @@ package com.example.demodeploy.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
